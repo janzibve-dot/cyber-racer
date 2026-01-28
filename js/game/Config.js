593220c4506721da1c8785a1a3ded9d3bde45acf
janzibve-dot <janzibve@gmail.com>
2026-01-29 00:15:18 +0500

    Update Config.js

--- js/game/Config.js
+++ js/game/Config.js
@@ -1,15 +1,17 @@
 export const CONFIG = {
     colors: {
-        sky: 0x000000,      // ТЕПЕРЬ ЧЕРНЫЙ ФОН (Проверка обновления)
+        sky: 0x000000,      // Черный фон
         fog: 0x000000,      // Черный туман
-        neonCyan: 0x00f3ff, 
-        neonPink: 0xff00ff, 
-        road: 0x111111      
+        neonCyan: 0x00f3ff, // Неон
+        neonPink: 0xff00ff, // Неон
+        road: 0x111111      // Асфальт
     },
     camera: {
         type: 'orthographic',
-        viewSize: 60,       // Увеличил масштаб, чтобы видеть больше (было 40)
-        position: { x: 0, y: 30, z: 30 }
+        viewSize: 50,       // Размер кадра
+        // ВАЖНО: Опустили камеру (y: 10) и отодвинули назад (z: 50)
+        // Это даст вид "в спину", а не сверху.
+        position: { x: 0, y: 10, z: 50 } 
     },
     speed: {
         start: 0,
@@ -17,7 +19,7 @@ export const CONFIG = {
         acceleration: 0.8   
     },
     road: {
-        width: 40,          // Сделал дорогу шире
+        width: 40,          
         length: 200         
     }
 };
