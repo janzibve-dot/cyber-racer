269cc1febb2003061dc3ced934e8f5afac2779b5
janzibve-dot <janzibve@gmail.com>
2026-01-28 18:48:08 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+// Этот файл будет отвечать за инициализацию 3D мира
+window.GameApp = {
+    init: function() {
+        console.log("CyberPunk World Initializing...");
+        // Здесь позже будет код Three.js
+        alert("System Message: Engine Link Established. Ready for 3D update.");
+    }
+};
